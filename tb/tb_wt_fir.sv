d962035772d615b71f3eeffb6f491dbc2f9a1fbe
borsch.v <borsch.v@micran.ru>
2019-06-17 12:52:39 +0700

    RTL implementation of IDWT core

--- tb/tb_wt_fir.sv
+++ tb/tb_wt_fir.sv
@@ -12,21 +12,24 @@ module tb_wt_fir;
   // ******************************************
   // ******************************************
   
+  `include "./../include/header_wavelets.svh"
+
   //--------------------------------------------------------------------------------------------------
   // Declaration parameters
   //--------------------------------------------------------------------------------------------------
 
-  localparam int pWIDTH = 12;
-  localparam int pORDER = 12;
-  localparam int cCOEFS [pORDER] = '{-5,  19,  2,  -130,  112,  399,  -532,  -927,  1291,  3076,  2025,  456};
+  localparam int pWIDTH = $size(pDB6_Lo_D[0]);
+  localparam int pORDER = $size(pDB6_Lo_D);
  
   //--------------------------------------------------------------------------------------------------
   // Declaration variables
   //--------------------------------------------------------------------------------------------------
   
-  logic           [7:0] arr          [128];
+  logic    [pWIDTH-1:0] farr         [$];
+  int                   fdat         ;
   logic           [3:0] cnt_ena      ;
   logic                 clk_ena      ;
+  int                   pF           ;
   //
   logic                 fir__iclk    ;
   logic                 fir__iclk_ena;
@@ -41,13 +44,7 @@ module tb_wt_fir;
   //--------------------------------------------------------------------------------------------------
 
   initial begin
-    for (int i = 0; i < $size(arr); i++) begin
-      arr[i] = 0;
-      //arr[i] = $urandom_range(0, 64);
-      //arr[i] = i;
-    end
-    //
-    arr[64] = '1;
+    
   end
   
   //--------------------------------------------------------------------------------------------------
@@ -58,9 +55,41 @@ module tb_wt_fir;
     cnt_ena <= cnt_ena + 1'b1;
     clk_ena <= &cnt_ena;
   end
+
+  // File read thread
+  initial begin
+    pF = $fopen("./../doc/math/to_hardware/wt_fir_input.dat", "r");
+    while (! $feof(pF)) begin
+      $fscanf(pF, "%f\n", fdat);
+      farr.push_back(fdat);
+      //$display(fdat);
+    end
+    $fclose(pF);
+  end
+
   // Main thread
   initial begin
-    
+    fir__irst = 1; #0.1us fir__irst = 0;
+    //
+    @(posedge clk_ena);
+    fir__iena = 1'b1;
+    foreach (farr[i]) begin
+      fir__idat = farr[i];
+      @(posedge clk_ena);
+      fir__idat = '0;
+      @(posedge clk_ena);
+    end
+    fir__iena = 1'b0;
+  end
+
+  // File write thread
+  initial begin
+    pF = $fopen("fir_resp.dat", "w");
+    #0.1us;
+  end
+  always@(posedge clk_ena) begin
+    if (fir__oena)
+      $fwrite(pF, "%d\n", $signed(fir__odat));
   end
 
   //--------------------------------------------------------------------------------------------------
@@ -69,19 +98,19 @@ module tb_wt_fir;
 
   wt_fir
   #(
-    . pWIDTH     (pWIDTH        ) ,
-    . pORDER     (pORDER        ) ,
-    . cCOEFS     (cCOEFS        )
+    . pWIDTH     (                 16) ,
+    . pORDER     (                 12) ,
+    . cCOEFS     (          pDB6_Lo_D)
   )
   fir__
   (
-    . iclk       (clk           ) ,
-    . iclk_ena   (clk_ena       ) ,
-    . irst       (fir__irst     ) ,
-    . iena       (fir__iena     ) ,
-    . idat       (fir__idat     ) ,
-    . oena       (fir__oena     ) ,
-    . odat       (fir__odat     )
+    . iclk       (clk                ) ,
+    . iclk_ena   (clk_ena            ) ,
+    . irst       (fir__irst          ) ,
+    . iena       (fir__iena          ) ,
+    . idat       (fir__idat          ) ,
+    . oena       (fir__oena          ) ,
+    . odat       (fir__odat          )
   );
 
 endmodule
